8e870f2d09831c6c909cac2fcb9c9efd7aeed406
Abdullahi <abdullahi.hagi98@gmail.com>
2022-06-21 12:42:41 -0400

    js

---
+++ m1_fundamentals/fundamental.js/first.js
@@ -0,0 +1,18 @@
+
+// alert  ("welcome");
+
+// console.log("welcome");
+
+let num1=10;
+let num2=5;
+let add= num1+num2;
+console.log(add);
+
+let sub = num1 - num2;
+console.log(sub)
+
+let mul = num1 * num2;
+console.log(mul);
+
+let div = num1 / num2;
+console.log(div);
