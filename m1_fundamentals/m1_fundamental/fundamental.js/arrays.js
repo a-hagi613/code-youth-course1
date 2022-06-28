c2bb2bb614f2711d84cb5d3f66e96640ff8a1b31
Abdullahi <abdullahi.hagi98@gmail.com>
2022-06-28 12:40:19 -0400

    js arrays

---
+++ m1_fundamentals/m1_fundamental/fundamental.js/arrays.js
@@ -0,0 +1,13 @@
+// let marks = [10,20,30,40,50];
+
+// console.log(marks[3]);
+
+// let fruit = ["apple", "orange", "banana", "mango"]
+// console.log("here is a list of fruits:",fruit);
+
+// Create an array with 3 names then console.log the array length
+
+let list = ["abdullahi", "brenda", "carter"];
+console.log(list.length)
+
+console.log(list[0])
