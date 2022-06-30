0b07b4684ffe8c66f30ff73e1b7c951f5d751315
Abdullahi <abdullahi.hagi98@gmail.com>
2022-06-30 09:26:07 -0400

    more arrays

--- m1_fundamentals/m1_fundamental/fundamental.js/arrays.js
+++ m1_fundamentals/m1_fundamental/fundamental.js/arrays.js
@@ -7,7 +7,61 @@
 
 // Create an array with 3 names then console.log the array length
 
-let list = ["abdullahi", "brenda", "carter"];
-console.log(list.length)
+// let list = ["abdullahi", "brenda", "carter"];
+// console.log(list.length)
 
-console.log(list[0])
+// console.log(list[0])
+
+// let arr1= [1,2,3,4,5,6,7,8,9];
+
+//  for (let i=0; i<arr1.length; i++) {
+//   console.log(arr1[i] * arr1[i])
+// };
+
+
+// let arr2= [1,2,3,4,5,6,7,8,9];
+// let num= 7
+
+//  for (let i=0; i<arr2.length; i++) {
+//   if (num=arr2[i]) {
+//     console.log(i)
+//   }
+// };
+
+
+// Remove an item from an array using pop() method//
+
+// const words =  ["cat", "dog"];
+// console.log(words.pop(1));
+
+// Question 4: Create the following array: ['burger', 'fries', 'coke',
+//       'donuts', 'salad', 'nuggets'], then remove the first 3 elements, and then
+//       add them back on at the end Hint: Variables are like boxes
+
+
+let food= ['burger', 'fries', 'coke','donuts', 'salad', 'nuggets']
+
+let food2= food.splice(0,3);
+
+console.log(food)
+
+let food3= food2.splice(1,2,'burger', 'fries', 'coke')
+console.log(food3)
+
+
+// array of numbers
+const numbers =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 19] ;
+
+const slicearr= numbers.splice(3,5);
+console.log(numbers);
+let newArr = numbers[]
+
+
+console.log(slicearr);
+
+
+
+
+
+
+
