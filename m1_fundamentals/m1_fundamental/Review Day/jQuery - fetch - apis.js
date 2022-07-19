7520bc9f18e2aa3f5d4e96ad71c2bef37381e0ec
a-hagi613 <abdullahi.hagi98@gmail.com>
2022-07-19 09:13:06 -0400

    .

---
+++ m1_fundamentals/m1_fundamental/Review Day/jQuery - fetch - apis.js
@@ -0,0 +1,89 @@
+////////////////////////////// ~ jQuery ~ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// Question 1
+/*
+Using jQuery create a div that has a width of 100%, height of 10rem, and a border
+Give it a class
+Append it to the body
+*/
+
+
+// Question 2
+/*
+Using jQuery add a keypress event listener to the body
+When you press a key append a p tag with the letter you typed into the div from question 1
+The p tag should display inline and have a font size of 10rem
+
+!Hint: remember (event)
+
+!!Hint: .originalEvent
+*/
+
+
+
+// Question 3
+/*
+    <table border="1" style="width: 5rem; height: 5rem" class="myTable">
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+
+    Copy and paste this HTML into your HTML file
+
+    Using jQuery put a random number from 0 - 10 inside each table cell
+
+    !Difficulty Tweak: Make it so no two cells can have the same number
+*/
+
+
+// Question 4
+/*
+<p class="copyMe">Click me to copy</p>
+
+copy and paste this into your html
+Add a click event listener
+Every time you click on the p tag the text should be copied to your clipboard
+(You will probably need to google)
+*/
+
+
+
+//////////////////////////////////// ~ fetch - apis ~ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// Question 1
+/* 
+'https://jsonplaceholder.typicode.com/users'
+
+fetch the link above and console.log the name of everyone that you get back
+*/
+
+
+// Question 2
+/*
+'https://jsonplaceholder.typicode.com/users'
+
+fetch the link above(you can use the fetch from the last question)
+For every name that starts with the letter C create a p tag with the name and append it to the body
+*/
+
+
+// Question 3
+/*
+'https://jsonplaceholder.typicode.com/users'
+
+fetch the link above(you can use the fetch from the last question)
+Create a small profile div that has all this information for each person:
+name
+phone number
+email
+city
+website(as a link)
+*/
+
+
